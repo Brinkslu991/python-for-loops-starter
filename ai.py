dadfbc2ef3d776c7abcf24a755f851618c617875
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-11-06 10:48:30 -0500

    FBGH

---
+++ ai.py
@@ -0,0 +1,7 @@
+# Lucas Brinks
+# ai
+# 11/6/24
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+secret_number = 7
+attemps = 3
--- main.py
+++ main.py
@@ -2,15 +2,26 @@
 # range practice
 # 11/4/24
 
-for i in range(1,20+1):
-    print(i)
-#for i in range(1,1000001):
-    #print(i)
-mill = [1,5,10,20,25,50,55,100,150,300,500,1200,5000,10000,50000,1000000]
-for i in range(mill):
-    min = min(i)
-    max = max(i)
-    sum = sum(i)
-    print(f'''The smallest number is {min}
-    The biggest number is {max}
-    The sum is {sum}''')
+#4-3
+# for i in range(1,20+1):
+#     print(i)
+#4-4/4-5
+# numbers = range(1000001)
+# for i in numbers:
+#     print(i)
+# minumbers = min(numbers)
+# manumbers = max(numbers)
+# sunumbers = sum(numbers)
+
+# print(f'{minumbers} {manumbers} {sunumbers}')
+#4-6
+# for i in range(1,20,2):
+#     print(i)
+#4-7
+# three = [3,6,9,12,15,18,21,24,27,30]
+# for i in three:
+#     print(i)
+#4-8
+# for i in range(1,10):
+#     cube = i**3
+#     print(cube)
